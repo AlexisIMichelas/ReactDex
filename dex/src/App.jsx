f80c00ce6ef084662c638fb7399dc02f05267e50
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-07-12 15:43:34 +0200

    finish exercice 3

--- dex/src/App.jsx
+++ dex/src/App.jsx
@@ -1,11 +1,30 @@
-import PokemonCard from "./components/PokemonCard"
+import './App.css'
+import PokemonCard from "./components/PokemonCard.jsx"
+
 
 function App() {
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
   return (
-    <div>
-      <PokemonCard />
-    </div>
+    <>
+      <div>
+      <PokemonCard pokemon={pokemonList[1]} />
+      </div>
+    </>
   )
 }
+
+
 
 export default App
--- dex/src/components/PokemonCard.jsx
+++ dex/src/components/PokemonCard.jsx
@@ -1,29 +1,23 @@
-import React from 'react';
+import styles from './PokemonCard.module.css';
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+function PokemonCard({ pokemon }) {
+    return <div className={styles.container}>
+            <figure> 
+                {pokemon.imgSrc === undefined 
+                    ? 
+                    <p>???</p> 
+                    :                 
+                    <img 
+                        src={pokemon.imgSrc}
+                        alt={pokemon.name}>
+                    </img>
+                }
 
-function PokemonCard() {
+                <figcaption className={styles.title} >{pokemon.name}</figcaption>
+            </figure>
+            </div>
+  }
 
-  const pokemon = pokemonList[0];  
 
-  return (
-    <div className="pokemon-card">
-      <h1>{pokemon.name}</h1>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
-    </div>
-  );
-}
-
-export default PokemonCard;
+  
+  export default PokemonCard;
